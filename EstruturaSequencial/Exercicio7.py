8ef2f35715e9fba064ac93346739996e5754cf21
ErikaGonzaga <erikagonzaga@alu.ufc.br>
2021-03-30 22:41:56 -0300

    teste commit

---
+++ EstruturaSequencial/Exercicio7.py
@@ -0,0 +1,7 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+
+lado_do_quadrado = int(input())
+area_do_quadrado = lado_do_quadrado ** 2
+dobro_da_area = 2 * area_do_quadrado
+
+print("O dobro da área do quadrado é:",dobro_da_area)
